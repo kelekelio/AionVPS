86d810cdabcc9c901a05df8569d09aab10144a59
kelekelio <cristal01@gmail.com>
2021-06-12 15:39:32 +0200

    Initial commit

---
+++ src/main/java/pl/aionpb/aion/db/controller/MainController.java
@@ -0,0 +1,21 @@
+package pl.aionpb.aion.db.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * @author Grzegorz Nowakowski
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+
+        return "index";
+    }
+
+
+}
---
+++ src/test/java/pl/aionpb/aion/AionApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.aionpb.aion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
